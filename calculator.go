67955bfc0cb49c84315ab4f1bb402236690a0352
Ross Macduff <ross@macduff.ca>
2021-01-09 18:51:58 -0500

    add support and test for variadic functions

--- calculator.go
+++ calculator.go
@@ -6,28 +6,45 @@ import (
 	"math"
 )
 
-// Add takes two numbers and returns the result of adding them together.
-func Add(a, b float64) float64 {
-	return a + b
-}
-
-// Subtract takes two numbers and returns the result of subtracting the second
-// from the first.
-func Subtract(a, b float64) float64 {
-	return a - b
-}
-
-// Multiply takes two numbers and returns the result of multiplyiing them together.
-func Multiply(a, b float64) float64 {
-	return a * b
-}
-
-// Divide takes two numbers and returns the restult of dividing the first by the second.
-func Divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, fmt.Errorf("bad input: %f, %f (division by zero is undefined)", a, b)
+// Add takes at least two numbers and returns the result of adding them together.
+func Add(inputs ...float64) float64 {
+	// var result float64
+	result := 0.0
+	for _, input := range inputs {
+		result += input
 	}
-	return a / b, nil
+	return result
+}
+
+// Subtract takes at least two numbers and returns the result of subtracting the second
+// from the first, then the 3rd from that result, and so on
+func Subtract(inputs ...float64) float64 {
+	result := inputs[0]
+	for i := 1; i < len(inputs); i++ {
+		result -= inputs[i]
+	}
+	return result
+}
+
+// Multiply takes at least two numbers and returns the result of multiplyiing them together.
+func Multiply(inputs ...float64) float64 {
+	result := 1.0
+	for _, input := range inputs {
+		result *= input
+	}
+	return result
+}
+
+// Divide takes at least two numbers and returns the restult of dividing the first by the second.
+func Divide(inputs ...float64) (float64, error) {
+	result := inputs[0]
+	for i := 1; i < len(inputs); i++ {
+		if inputs[i] == 0 {
+			return 0, fmt.Errorf("bad input: inputs[i]  (division by zero is undefined)")
+		}
+		result /= inputs[i]
+	}
+	return result, nil
 }
 
 // Takes a number and returns the square root of it.
--- calculator_test.go
+++ calculator_test.go
@@ -10,21 +10,33 @@ func TestAdd(t *testing.T) {
 	t.Parallel()
 
 	type testCase struct {
-		a, b float64
-		want float64
+		name    string
+		a, b, c float64
+		want    float64
 	}
 
-	testCases := []testCase{
-		{a: 2, b: 2, want: 4},
-		{a: 1, b: 1, want: 2},
-		{a: 5, b: 0, want: 5},
+	testCases2Inputs := []testCase{
+		{name: "Add two integers", a: 2, b: 2, want: 4},
+		{name: "Add a positive integer and zero", a: 5, b: 0, want: 5},
 	}
 
-	for _, tc := range testCases {
+	for _, tc := range testCases2Inputs {
 		got := calculator.Add(tc.a, tc.b)
 		if tc.want != got {
 			t.Errorf("Add(%f, %f): want %f, got %f",
 				tc.a, tc.b, tc.want, got)
+		}
+	}
+
+	testCases3Inputs := []testCase{
+		{name: "Add three integers", a: 1, b: 2, c: 3, want: 6},
+	}
+
+	for _, tc := range testCases3Inputs {
+		got := calculator.Add(tc.a, tc.b, tc.c)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f, %f): want %f, got %f",
+				tc.a, tc.b, tc.c, tc.want, got)
 		}
 	}
 }
@@ -33,21 +45,34 @@ func TestSubtract(t *testing.T) {
 	t.Parallel()
 
 	type testCase struct {
-		a, b float64
-		want float64
+		name    string
+		a, b, c float64
+		want    float64
 	}
 
-	testCases := []testCase{{a: 4, b: 2, want: 2},
-		{a: 0, b: 2, want: -2},
-		{a: 1, b: 0.5, want: 0.5},
-		{a: -1, b: -1, want: 0},
-		{a: -1, b: 1, want: -2},
+	testCases2Inputs := []testCase{
+		{name: "Subtract smaller from larger", a: 4, b: 2, want: 2},
+		{name: "Subtract larger from smaller, expect negative", a: 0, b: 2, want: -2},
+		{name: "Subtract fraction from integer", a: 1, b: 0.5, want: 0.5},
+		{name: "Subtract two negative numbers", a: -1, b: -1, want: 0},
+		{name: "Subtract a positive number from a negative numer", a: -1, b: 1, want: -2},
 	}
 
-	for _, tc := range testCases {
+	for _, tc := range testCases2Inputs {
 		got := calculator.Subtract(tc.a, tc.b)
 		if tc.want != got {
-			t.Errorf("want %f, got %f", tc.want, got)
+			t.Errorf("%s: want %f, got %f", tc.name, tc.want, got)
+		}
+	}
+
+	testCases3Inputs := []testCase{
+		{name: "Subtract three numbers", a: 4, b: 2, c: 1, want: 1},
+	}
+
+	for _, tc := range testCases3Inputs {
+		got := calculator.Subtract(tc.a, tc.b, tc.c)
+		if tc.want != got {
+			t.Errorf("%s: want %f, got %f", tc.name, tc.want, got)
 		}
 	}
 }
@@ -56,12 +81,12 @@ func TestMultiply(t *testing.T) {
 	t.Parallel()
 
 	type testCase struct {
-		name string
-		a, b float64
-		want float64
+		name    string
+		a, b, c float64
+		want    float64
 	}
 
-	testCases := []testCase{
+	testCases2Inputs := []testCase{
 		{name: "Two positive integers whose product is a positive integer", a: 4, b: 2, want: 8},
 		{name: "Multiplying anythin by 0 produces a 0", a: 0, b: 2, want: 0},
 		{name: "Multiply anything by 1 and get anything", a: 1, b: 2, want: 2},
@@ -69,8 +94,19 @@ func TestMultiply(t *testing.T) {
 		{name: "Multiply integer by fractional float", a: 0.5, b: 4, want: 2},
 	}
 
-	for _, tc := range testCases {
+	for _, tc := range testCases2Inputs {
 		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("%s: want %f, got %f", tc.name, tc.want, got)
+		}
+	}
+
+	testCases3Inputs := []testCase{
+		{name: "Multiply 3 positive integers", a: 5, b: 3, c: 2, want: 30},
+	}
+
+	for _, tc := range testCases3Inputs {
+		got := calculator.Multiply(tc.a, tc.b, tc.c)
 		if tc.want != got {
 			t.Errorf("%s: want %f, got %f", tc.name, tc.want, got)
 		}
@@ -82,20 +118,37 @@ func TestDivide(t *testing.T) {
 
 	type testCase struct {
 		name        string
-		a, b        float64
+		a, b, c     float64
 		want        float64
 		errExpected bool
 	}
 
-	testCases := []testCase{
+	testCases2Inputs := []testCase{
 		{name: "Divide two positive integers", a: 8, b: 2, want: 4, errExpected: false},
 		{name: "Divide two negative integers", a: -8, b: -2, want: 4, errExpected: false},
 		{name: "Divide one positive and one negative integer", a: -8, b: 2, want: -4, errExpected: false},
 		{name: "Divide by 0", a: 8, b: 0, want: 0, errExpected: true},
 	}
 
-	for _, tc := range testCases {
+	for _, tc := range testCases2Inputs {
 		got, err := calculator.Divide(tc.a, tc.b)
+
+		errReceived := err != nil
+		if tc.errExpected != errReceived {
+			t.Fatalf("Divide(%f, %f): unexpected error status: %v", tc.a, tc.b, errReceived)
+		}
+
+		if !tc.errExpected && tc.want != got {
+			t.Errorf("%s: want %f, got %f", tc.name, tc.want, got)
+		}
+	}
+
+	testCases3Inputs := []testCase{
+		{name: "Divide three positive integers", a: 16, b: 4, c: 2, want: 2, errExpected: false},
+	}
+
+	for _, tc := range testCases3Inputs {
+		got, err := calculator.Divide(tc.a, tc.b, tc.c)
 
 		errReceived := err != nil
 		if tc.errExpected != errReceived {
